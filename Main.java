716ae0849a74aa339a8cf8cb451e1e403046c9c8
Zita Koczka <koczka-z@ulster.ac.uk>
2020-02-11 13:33:11 +0000

    BinarySearch java file

---
+++ Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Zita Cathcart
+ */
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] list = new int[1000];
+        for (int i = 0; i < list.length; i++ ) {
+            list[i] = i;
+        }
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("What value are you looking for? ");
+        int value = Integer.parseInt(reader.nextLine());
+        System.out.println("The value you're looking for is at position " + FindNum(list, value));
+    }
+
+    public static int FindNum(int[] list, int value) {
+        int left = 0;
+        int middle = list.length / 2;
+        int right = list.length;
+        int counter = 0;
+
+        while (left <= middle && right >= middle) {
+            counter++;
+            if (list[middle] == value) {
+                System.out.println("It took " + counter + " number of searches to find the location.");
+                return middle;
+            } else if (list[middle] < value) {
+                left = middle + 1;
+                middle = (left + right) / 2;
+            } else {
+                right = middle - 1;
+                middle = (left + right) / 2;
+            }
+        } System.out.println("It took " + counter +  " number of searches to find the location.");
+        return -1;
+    }
+
+}
